3a8c925f8cf2c2d59d7d81487bdb33d2d2f1efa0
Giovana do Nascimento <giovana.np1@gmail.com>
2020-04-22 00:14:24 -0300

    update e get produtos

--- Controllers/ProdutosController.js
+++ Controllers/ProdutosController.js
@@ -15,17 +15,37 @@ module.exports = {
             image,
             size
         })
-        console.log(produto)
+        console.log('POST /produto Produto.create', request)
         return response.json(produto)
     },
     async show (request, response){
 
     },
     async index (request,  response){
-
+        const produto = await Produto.find();
+        console.log('GET /produto Produto.index', request)
+        return response.json(produto);
     },
-    async update (request, response){
+    async update (req, response){
+        var _id = req.body._id
 
+        Produto.findById(_id, function(err, doc) {
+        if (err){
+            console.error('error, no entry found');  
+        }
+        doc.name = req.body.name,
+        doc.type = req.body.type,
+        doc.price = req.body.price,
+        doc.rarity = req.body.rarity,
+        doc.description = req.body.description,
+        doc.region = req.body.region,
+        doc.spawnTimes = req.body.spawnTimes,
+        doc.image = req.body.image,
+        doc.size = req.body.size,
+        doc.save();
+        })
+        console.log('PUT /produto Produto.update', req.body)
+        return response.json(req.body)
     },
     async destroy (request, response){
 
--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const routes = Router();
 const ProdutoController = require('./Controllers/ProdutosController')
 
 
+ routes.get('/produto', ProdutoController.index);
  routes.post('/produto', ProdutoController.create);
+ routes.put('/produto', ProdutoController.update);
+
 
  module.exports = routes

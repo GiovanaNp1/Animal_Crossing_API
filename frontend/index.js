93bec748b71b69b7bc3b35ad6cfc7584e71e40a9
Giovana do Nascimento <giovana.np1@gmail.com>
2020-06-02 20:48:03 -0300

    Ajust for deploy cloud

--- frontend/index.js
+++ frontend/index.js
@@ -6,7 +6,7 @@ const port = (process.env.PORT || 8080);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '/frontend')));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
   

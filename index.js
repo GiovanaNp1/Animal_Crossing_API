f8d9ef47ab09e60d9e16a3ff27ec22926af9816d
Giovana do Nascimento <giovana.np1@gmail.com>
2020-06-02 20:42:12 -0300

    Ajust for deploy cloud

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ mongoose.connect('mongodb+srv://Visitante:Visitante123@cluster0-elgva.mongodb.ne
 });
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/frontend')));
 app.use(routes)
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
